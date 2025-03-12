556ffad2ce9e0a2fae0516fee77cdc8b5e9ab738
chuacn1 <chuacn1@student.op.ac.nz>
2025-03-12 13:54:53 +1300

    Array.Sort(), Array.Reverse().ToArray
    Create an array of integers called nums with the values 21, 19, 68, 55, 42, 12. Iterate over the array and check if each number is odd. If it is, display it. Finally, sort the array from lowest to highest, then display again.

---
+++ ArrayOddSort/ArrayOddSort/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayOddSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 21, 19, 68, 55, 42, 12 };
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
+
+            Array.Sort(nums); //=============================> does bubble sort in ascending order
+                              //=============================> if wanna do descending order, do Array.Sort(ascending) and then Array.Reverse(ascending).ToArray();
+
+            Console.WriteLine("\nSorted array:");
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    }
+}
