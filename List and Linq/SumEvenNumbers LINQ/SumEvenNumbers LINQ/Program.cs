1aedc08d5e7fe7ad0140e8b5ccb7b8842361d214
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 20:39:35 +1300

    m

---
+++ List and Linq/SumEvenNumbers LINQ/SumEvenNumbers LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SumEvenNumbers_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 4, 7, 8, 11, 12, 15, 16, 19, 20 };
+
+            IEnumerable<int> query = numbers.Where(num => num % 2 == 0);             // or just do this: int sumOfEvenNumbers = numbers.Where(num => num % 2 == 0).Sum();
+                                                                                     //.Select(num => num);    .Select is if you want to change the values in the collection
+
+            int total = 0;
+            foreach (int num in query)
+            {
+                total += num;
+            }
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
