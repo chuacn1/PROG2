09de792a88c34d2fe20bb45b9512d126c891c31b
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 20:50:50 +1300

    m

---
+++ List and Linq/CityPopulation Analysis/CityPopulation Analysis/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CityPopulation_Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> cityPopulations = new List<int>() { 5000000, 3000000, 1200000, 8000000, 2000000, 4500000, 6000000 };
+            IEnumerable<int> top3 = cityPopulations.OrderByDescending(top => top).Take(3).ToList();
+
+            int total = cityPopulations.Sum();
+
+            Console.WriteLine(total);
+        }
+    }
+}
