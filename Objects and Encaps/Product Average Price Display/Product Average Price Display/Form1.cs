262ee1700fcf779382bde9eddc02f25f9a5578f8
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 19:22:09 +1300

    Write a LINQ query that displays the average price of all products in the list of Product in a Label.

---
+++ Objects and Encaps/Product Average Price Display/Product Average Price Display/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product_Average_Price_Display
+{
+    public partial class Form1 : Form
+    {
+        private static List<Product> products; 
+        public Form1()
+        {
+            InitializeComponent();
+
+            products = new List<Product>() 
+            {
+                new Product("Apple", 1.99),
+                new Product("Banana", 2.99),
+                new Product("Orange", 3.99)
+            };
+
+            double averagePrice = products.Average(p => p.Price);
+
+            label1.Text = $"Average Price: ${averagePrice:F2}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Objects and Encaps/Product Average Price Display/Product Average Price Display/Product.cs
@@ -0,0 +1,18 @@
+﻿// Create a new file called Product.cs. Copy and paste the following code into it
+using System.Collections.Generic;
+
+public class Product
+{
+    private string name;
+    private double price;
+
+    public Product(string name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    public string Name { get => name; set => name = value; }
+    public double Price { get => price; set => price = value; }
+}
+
