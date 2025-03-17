130d3f5cf770041b61b6dd4e5c7acbe0c2dff936
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 18:39:09 +1300

    =

---
+++ Objects and Encaps/Desk.cs/Desk.cs/Chair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desk.cs
+{
+    public class Chair
+    {
+        public string material;
+        public string color;
+        public int height;
+
+        public Chair (string material, string color, int height)
+        {
+            this.material = material;
+            this.color = color;
+            this.height = height;
+        }
+
+        public string DisplayInfo()
+        {
+            return $"Chair Information:\nMaterial: {material}\nColor: {color}\nHeight: {height} cm";
+        }
+
+
+
+    }
+}
