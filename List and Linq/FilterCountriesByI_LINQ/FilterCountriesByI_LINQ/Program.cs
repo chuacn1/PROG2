1d02377e7c035634cc6b94c15aca8ebd315cc351
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 20:43:12 +1300

    m

---
+++ List and Linq/FilterCountriesByI_LINQ/FilterCountriesByI_LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class FilterCountriesByI_LINQ
+{
+    static void Main(string[] args)
+    {
+        List<string> countries = new List<string>
+        {
+            "Argentina",
+            "Australia",
+            "Brazil",
+            "Canada",
+            "Egypt",
+            "France",
+            "India",
+            "Italy",
+            "Mexico",
+            "Netherlands",
+            "South Africa",
+            "United States",
+        };
+
+
+        IEnumerable<string> query = countries.Where(country => country.StartsWith("I", StringComparison.OrdinalIgnoreCase));
+
+        // The StartsWith("I", StringComparison.OrdinalIgnoreCase) method checks if the country starts with the letter "I" or "i", ignoring the case.
+        //The StringComparison.OrdinalIgnoreCase makes the comparison case -insensitive, so both "I" and "i" will match.
+
+        foreach (string country in query)
+        {
+            Console.WriteLine(country);
+        }
+    }
+}
