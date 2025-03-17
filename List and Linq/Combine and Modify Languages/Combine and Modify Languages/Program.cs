e1f68d4f22770f4976312d6cb0836813b50517ce
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 20:34:07 +1300

    =

---
+++ List and Linq/Combine and Modify Languages/Combine and Modify Languages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combine_and_Modify_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> progLangsOne = new List<string>() { "C#", "JavaScript", "Kotlin", "Python" };
+            List<string> progLangsTwo = new List<string>() { "C++", "Go", "Swift", "TypeScript" };
+
+            progLangsTwo.AddRange(progLangsOne);
+
+            List<string> allProgLangs = progLangsTwo;
+
+            allProgLangs.Add("Rust");
+            allProgLangs.Remove("Swift");
+
+            foreach (string s in allProgLangs)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ List and Linq/InsertContainsFindSortIntegers/InsertContainsFindSortIntegers/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace InsertContainsFindSortIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 10, 20, 30, 40, 50 };
+
+
+            //=================INSERT================//
+            nums.Insert(1, 25);
+            Console.WriteLine(string.Join(", ", nums));              // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//                          
+            Console.ReadLine();
+
+            Console.WriteLine();
+
+
+            //=================CONTAINS================//
+            bool hasNumber35 = nums.Contains(35);
+            Console.Write(hasNumber35);
+            Console.ReadLine();
+
+            Console.WriteLine();
+
+
+            //=================FIND================//
+            int firstNumberGreaterThan30 = nums.Find(num => num > 30);
+            Console.Write(firstNumberGreaterThan30);
+            Console.ReadLine();
+
+            Console.WriteLine();
+
+            //=================SORT================//
+            nums.Sort();
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
