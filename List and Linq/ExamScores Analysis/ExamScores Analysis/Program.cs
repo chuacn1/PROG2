22489de7b29c1c2e3ced1dc3efb40ce7a984724c
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 20:48:40 +1300

    m

---
+++ List and Linq/ExamScores Analysis/ExamScores Analysis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Schema;
+
+namespace ExamScores_Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> scores = new List<int>() { 78, 89, 92, 65, 70, 85, 92, 78, 93, 80 };
+            List<int> noDuplicatedScores = scores.Distinct().ToList();                      //It compares each element in the collection and removes any duplicates, keeping only one occurrence of each element.
+
+            int highestScore = scores.Max();
+
+            Console.WriteLine("Scores without duplicates: " + string.Join(", ", noDuplicatedScores));
+            Console.WriteLine("Highest Score: " + highestScore);
+
+            Console.ReadLine();
+
+        }
+    }
+}
