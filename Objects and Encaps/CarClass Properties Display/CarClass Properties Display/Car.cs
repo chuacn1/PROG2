06a2d08f993faac26894ea85d1a9cc1d4fb33232
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-17 18:58:47 +1300

    Create a Car class with private fields: make, model, and year. Add public properties for Make, Model, and Year. Include a constructor that sets the make, model, and year when the object is created. Create three Car objects and display their Make, Model, and Year properties in a Label.

---
+++ Objects and Encaps/CarClass Properties Display/CarClass Properties Display/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass_Properties_Display
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year; 
+
+        public Car (string make, string model, int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+    }
+}
---
+++ Objects and Encaps/CarClass Properties Display/CarClass Properties Display/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarClass_Properties_Display
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Car car = new Car("Toyota", "Camry", 2020);
+            label2.Text = $"{car.Make} {car.Model} {car.Year}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
